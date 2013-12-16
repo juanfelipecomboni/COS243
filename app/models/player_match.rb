5e54faba548e1575bd9e5cc3ff7662291b7b04be
Juan Comboni <juan_comboni@taylor.edu>
2013-12-16 16:39:34 +0000

    Final Push

--- app/models/player_match.rb
+++ app/models/player_match.rb
@@ -3,4 +3,8 @@ class PlayerMatch < ActiveRecord::Base
   belongs_to :match
   validates :match, presence: true
   validates :player, presence: true
+  #default_scope ->{order"players_matches.scores DESC"}
+  #scope :wins,-> {where(results: 'Win')}
+  #scope :loses,-> {where(results: 'loses')}
+  
 end
